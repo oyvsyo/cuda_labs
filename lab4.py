96194dc56bce3dca7db77a38cc39fd37753dba32
oyvsyo <dvoiak.stepan@gmail.com>
2019-01-02 18:15:08 +0200

    initial

---
+++ lab4.py
@@ -0,0 +1,176 @@
+import ROOT
+import numpy as np
+
+
+Culon = ROOT.TF1("Culon", "    [0]*[1]*14.4/x*[2] ")
+dCulon = ROOT.TF1("dCulon", "  [0]*[1]*14.4/x* (  [2]/[3] - [4]/x )")
+
+fEcran = ROOT.TF1("fEcran", "[0]*exp(-[1]*x) + [2]*exp(-[3]*x) + [4]*exp(-[5]*x) + [6]*exp(-[7]*x)")
+dfEcran = ROOT.TF1("dfEcran", "-([0]*[1]*exp(-[1]*x) + [2]*[3]*exp(-[3]*x) + [4]*[5]*exp(-[5]*x) + [6]*[7]*exp(-[7]*x) )")
+
+f = ROOT.TF1("f", "1-[0]/[1]-([2]/x)*([2]/x) ")
+fstrih = ROOT.TF1("fstrih", "-[0]/[1]+2*[2]*[2]/(x*x*x)")
+
+ecran_params = [0.1818, 3.2, 0.5099, 0.9432, 0.2802, 0.4029, 0.0282, 0.2016]
+map(lambda iparam: fEcran.SetParameter(iparam, ecran_params[iparam]), range(8))
+map(lambda iparam:dfEcran.SetParameter(iparam, ecran_params[iparam]), range(8))
+
+
+def sintheta(Z1, Z2, M1, M2, E, p, method=0):
+    a = 0.88534 * 0.529 / (pow(Z1, 0.23) + pow(Z2, 0.23))
+    a = a * (-0.003 * (pow(Z1, 0.5) + pow(Z2, 0.5)) + 0.98)
+    if p == 0: p = 0.0001
+    B = p / a
+    Ec = E / (1 + M1 / M2)
+    rm = a
+
+    Culon.SetParameter(0, Z1)
+    Culon.SetParameter(1, Z2)
+    Culon.SetParameter(2, fEcran(rm / a))
+
+    dCulon.SetParameter(0, Z1)
+    dCulon.SetParameter(1, Z2)
+    dCulon.SetParameter(3, a)
+
+    f.SetParameter(1, Ec)
+    f.SetParameter(2, p)
+    f.SetParameter(0, Culon(rm))
+
+    fstrih.SetParameter(1, Ec)
+    fstrih.SetParameter(2, p)
+
+    # for gggg in range(10):
+    while(abs(f(rm))>0.0001):
+        Culon.SetParameter(2, fEcran(rm/a))
+        dCulon.SetParameter(2, dfEcran(rm/a))
+        dCulon.SetParameter(4, fEcran(rm/a))
+        f.SetParameter(0, Culon(rm))
+        fstrih.SetParameter(0, dCulon(rm))
+        rm = rm-f(rm)/fstrih(rm)
+    print(rm)
+    ro = -2 * (Ec - Culon(rm)) / (dCulon(rm)+1)
+
+    Rm = rm / a
+    Rc = ro / a
+    Esm = M2 / (M1 + M2) * E
+    epsilon = a * Esm / (Z1 * Z2 * 14.4)
+    alfa = 1 + 0.75984 * pow(epsilon, -0.5)
+    beta = (5.71974 + pow(epsilon, 0.5)) / (6.14171 + pow(epsilon, 0.5))
+    gamma = (9.5217 + epsilon) / (6.2612 + epsilon)
+    A = 2 * alfa * epsilon * pow(B, beta)
+    G = gamma / (pow((1 + A * A), 0.5) - A)
+
+    delta = A * (Rm - B) / (1 + G)
+    if method == 0:  # coulomb
+        return 1/(1+2*epsilon*B*2*epsilon*B)
+
+    # semi-empirical
+    if method == 1:
+        if (B + Rc + delta) / (Rm + Rc) < 1 and (B + Rc + delta) / (Rm + Rc) > -1:
+            theta = 2 * np.arccos((B + Rc + delta) / (Rm + Rc))
+        else:
+            # print B
+            return 1/(1+2*epsilon*B*2*epsilon*B)
+        return np.sin(theta/2)*np.sin(theta/2)
+
+    if B <= 1:
+        theta = (1.012 / B) * np.exp(-0.356 * B) / epsilon
+    if B > 1:
+        theta = 0.707 * pow(B, -1.78) / epsilon
+
+    # impulse
+    if method == 2:
+        return np.sin(theta / 2)**2
+
+
+if __name__ == "__main__":
+    Z1 = 1.
+    Z2 = 3.
+    M1 = 3.016
+    M2 = 6.015
+
+    E = [1, 10, 100, 1000, 10000]
+    n = 100
+    Bmax = 2. - 0.001
+
+    X = np.zeros(n)
+    Y = np.zeros(n)
+    impY = np.zeros(n)
+    couY = np.zeros(n)
+
+    a = 0.88534 * 0.529 / (pow(Z1, 0.23) + pow(Z2, 0.23))
+    a = a * (-0.003 * (pow(Z1, 0.5) + pow(Z2, 0.5)) + 0.98)
+
+    text_C = "Z_{1}=%1i, M_{1}=%1.1f amu, Z_{2}=%1i, M_{2}=%1.1f amu" % (Z1, M1, Z2, M2)
+    c1 = ROOT.TCanvas("c1", "c", 1200, 1000)
+
+
+    ROOT.gStyle.SetOptStat(0)
+    pad1 = ROOT.TPad("pad1", "pad1", 0.0, 0.5, 0.33, 1.0)
+    pad2 = ROOT.TPad("pad2", "pad2", 0.33, 0.5, 0.66, 1.0)
+    pad3 = ROOT.TPad("pad3", "pad3", 0.66, 0.5, 1.0, 1.0)
+    pad4 = ROOT.TPad("pad4", "pad4", 0.0, 0.0, 0.33, 0.5)
+    pad5 = ROOT.TPad("pad5", "pad5", 0.33, 0.0, 1.0, 0.5)
+    pads = [pad1, pad2, pad3, pad4, pad5]
+    map(lambda pad: pad.Draw(), pads)
+
+    gr = range(5)
+    couGr = range(5)
+    impGr = range(5)
+
+    for iE in range(5):
+        pads[iE].cd()
+
+        step = Bmax*a/n
+        p = 0+0.001
+
+        for j in range(n):
+            p=step*j
+            B=p/a+0.001
+            X[j] = B
+            Y[j] = sintheta(Z1, Z2, M1, M2, E[iE], p, method=1)
+            impY[j] = sintheta(Z1, Z2, M1, M2, E[iE], p, method=2)
+            couY[j] = sintheta(Z1, Z2, M1, M2, E[iE], p, method=0)
+
+        text_A = "E=%1.0f keV" % E[iE]
+
+        if Z1 == 1 and Z2 == 22: text_B = "D #RightarrowTi "
+        if Z1 == 2 and Z2 == 6: text_B = "{}^{4}He #Rightarrow{}^{12}C "
+        if Z1 == 3 and Z2 == 14: text_B = "{}^{7}Li #RightarrowSi"
+        if Z1 == 1 and Z2 == 8: text_B = "p #Rightarrow{}^{16}O"
+        if Z1 == 6 and Z2 == 18: text_B = "{}^{12}C #Rightarrow{}^{41}Ar"
+        if Z1 == 6 and Z2 == 18: text_B = "{}^{12}C #Rightarrow{}^{41}Ar"
+        if Z1 == 1 and Z2 == 3: text_B = "T #Rightarrow{}^{6}Li"
+
+        gr[iE] = ROOT.TGraph(j-1, X, Y)
+        gr[iE].SetTitle(text_A)
+        gr[iE].GetXaxis().SetTitle("B")
+        gr[iE].GetYaxis().SetTitle("sin^{2}(#theta/2)")
+        gr[iE].SetMarkerStyle(8)
+        gr[iE].SetMarkerSize(0.5)
+        gr[iE].Draw("AP")
+
+        impGr[iE] = ROOT.TGraph(j-1, X, impY)
+        impGr[iE].SetMarkerColor(2)
+        impGr[iE].SetMarkerColor(4)
+        impGr[iE].SetMarkerStyle(8)
+        impGr[iE].SetMarkerSize(0.3)
+        impGr[iE].Draw("P")
+
+        couGr[iE] = ROOT.TGraph(j-1, X, couY)
+        couGr[iE].SetMarkerColor(3)
+        couGr[iE].SetLineColor(3)
+        couGr[iE].SetMarkerColor(3)
+        couGr[iE].SetMarkerStyle(8)
+        couGr[iE].SetMarkerSize(0.3)
+        couGr[iE].Draw("P")
+
+    leg = ROOT.TLegend(0.4, 0.4, 0.8, 0.85)
+    leg.AddEntry(couGr[iE], text_B, "s")
+    leg.AddEntry(couGr[iE], text_C, "s")
+    leg.AddEntry(gr[iE], "Semi-empirical formula", "p")
+    leg.AddEntry(impGr[iE], "Impulse approximation", "p")
+    leg.AddEntry(couGr[iE], "Coulomb scattering", "p")
+    leg.Draw()
+
+    c1.Update()
---
+++ lab9.py
@@ -0,0 +1,204 @@
+from __future__ import division
+import numpy as np
+import ROOT
+import lab4
+
+
+Na = 6.02e6
+
+
+def length(x1, y1, z1, x2, y2, z2):
+    return (x2-x1)**2+(y2-y1)**2+(z2-z1)**2
+
+def calc_p(n0):
+    return (np.random.uniform(0., 1., 1)[0] / n0 ** (-2. / 3) / np.pi) ** 0.5
+
+
+def nap_cos(omega0, theta, phi):
+    omega1 = list(range(3))
+    mu = np.cos(theta)
+    omega1[0] = omega0[0] * mu - (omega0[1] * np.sin(phi) + omega0[0] * omega0[2] * np.cos(phi)) * ((1 - mu ** 2) / (1 - omega0[2] ** 2)) ** .5
+    omega1[1] = omega0[1] * mu + (omega0[0] * np.sin(phi) - omega0[1] * omega0[2] * np.cos(phi)) * ((1 - mu ** 2) / (1 - omega0[2] ** 2)) ** .5
+    omega1[2] = omega0[2] * mu + (1 - omega0[2] ** 2) * np.cos(phi) * ((1 - mu ** 2) / (1 - omega0[2] ** 2)) ** .5
+    return omega1
+
+
+class Atom(object):
+    """Class for Atom"""
+    def __init__(self, Z=1., M=2., name="-"):
+        self.Z = Z
+        self.M = M
+        self.name = name
+
+
+class Particle(Atom):
+    """Class for particle"""
+    def __init__(self, X=[0, 0, 0], Omega=[1., 0., 0.], E=100., M=2., Z=1., name="-"):
+        super(Particle, self).__init__(Z, M, name)
+        self.X = X
+        self.E = E
+        self.Omega = Omega
+
+
+class Molequla:
+    """Class for molequle"""
+    def __init__(self, atoms=[Atom()], weights=[]):
+        self.atoms = atoms
+        self.weights = weights
+        self.probability = np.array(weights)/float(sum(weights))
+        names = ""
+        for i in range(len(atoms)):
+            if weights[i] == 1:
+                names += atoms[i].name
+            else:
+                names += "%s_{%d}" % (atoms[i].name, weights[i])
+        self.name = names
+
+    def __str__(self):
+        return self.name
+
+
+class Environ:
+    """Class for enviroument"""
+    def __init__(self, molequla, ro=1.):
+        self.molequla = molequla
+        self.n0s = list(map(lambda i: ro*Na/molequla.atoms[i].M*molequla.weights[i], list(range(len(molequla.atoms)))))
+        self.Ls = list(map(lambda n0: n0 ** (-1. / 3), self.n0s))
+        self.ro = ro
+
+    def inter_partner(self):
+        return np.random.choice(self.molequla.atoms,
+                                1,
+                                p=self.molequla.probability)[0]
+
+    def do_interact(self, particle):
+        partner = self.inter_partner()
+        index = self.molequla.atoms.index(partner)
+        n0 = self.n0s[index]
+        L = self.Ls[index]
+        p = calc_p(n0)
+        M1, Z1 = particle.M, particle.Z
+        M2, Z2 = partner.M, partner.Z
+        E = particle.E
+        Emax = 4*M1*M2*E/(M1+M2)
+        sin2 = lab4.sintheta(Z1, Z2, M1, M2, E, p, method=0)
+
+        dE = Emax*sin2
+        # new energy
+        particle.E -= dE
+
+        cost = 1 - 2*sin2
+        sint = 1 - cost**2
+        theta_lab = np.arctan(sint/(cost+M1/M2))
+        phi = np.random.uniform(0, 1, 1)[0]*2*np.pi
+        # new Omega
+        particle.Omega = nap_cos(particle.Omega, theta_lab, phi)
+
+        particle.X = [
+            particle.X[0] + particle.Omega[0] * L,
+            particle.X[1] + particle.Omega[1] * L,
+            particle.X[2] + particle.Omega[2] * L
+        ]
+        return index
+
+
+H = Atom(name="H")
+He = Atom(2., 4., name="He")
+C = Atom(6., 12.01, name="C")
+F = Atom(9., 18., name="F")
+O = Atom(8., 16., name="O")
+Si = Atom(14., 28.086, name="Si")
+Li = Atom(3., 6.94, name="Li")
+Fe = Atom(26., 55.84, name="Fe")
+Ti = Atom(22., 47.86, name="Ti")
+T = Atom(1., 3., name="T")
+
+d = Particle(M=2., Z=1., E=110, name="d")
+alpha = Particle(Z=2, M=4., E=100, name="alpha")
+pLi = Particle(Z=3., M=6.94, E=90, name="Li")
+p = Particle(Z=1., M=1., E=70, name="p")
+t = Particle(Z=1., M=3., E=200, name="t")
+
+CH2 = Molequla([C, H], [1, 2])
+SiO2 = Molequla([Si, O], [1, 2])
+Fe2O3 = Molequla([Fe, O], [2, 3])
+LiFe = Molequla([Li, Fe], [1, 1])
+C12H18O7 = Molequla([C, H, O], [12, 18, 7])
+TiT2 = Molequla([Ti, T], [1, 2])
+LiF = Molequla([Li, F], [1, 1])
+
+
+P = p
+Env = Environ(TiT2, 1.)
+
+E = P.E
+n = 100
+natoms = len(Env.molequla.atoms)
+vlth = []
+lth = []
+
+# X, Y, Z = range(n), range(n), range(n)
+# for i in range(n):
+#     X[i] = []
+#     Y[i] = []
+#     Z[i] = []
+#     l = 0
+#     while P.E > 0:
+#         x, y, z = P.X[0], P.X[1], P.X[0]
+#         Env.do_interact(P)
+#         x1, y1, z1 = P.X[0], P.X[1], P.X[0]
+#         l += length(x, y, z, x1, y1, z1)
+#         print l
+#         X[i] += [P.X[0]]
+#         Y[i] += [P.X[1]]
+#         Z[i] += [P.X[2]]
+#     lth += [l]
+#     vlth += [length(X[i][0], Y[i][0], Z[i][0], X[i][-1], Y[i][-1], Z[i][-1])]
+#     print l, "   ", vlth[i]
+#     P.E = E
+#
+# h1 = ROOT.TH1F("g", "g", 50, 0., .5)
+# map(lambda x: h1.Fill(x), lth)
+# h1.Draw()
+X, Y, Z = list(range(natoms)), list(range(natoms)), list(range((natoms)))
+graps = list(range(natoms))
+x, y, z = [], [] ,[]
+while P.E > 0:
+    index = Env.do_interact(P)
+    if type(X[index])==type(1):
+        X[index] = []
+    if type(Y[index])==type(1):
+        Y[index] = []
+    if type(Z[index])==type(1):
+        Z[index] = []
+    x += [P.X[0]]
+    y += [P.X[1]]
+    z += [P.X[2]]
+    X[index] += [P.X[0]]
+    Y[index] += [P.X[1]]
+    Z[index] += [P.X[2]]
+
+
+gr = ROOT.TGraph2D(len(x), np.array(x), np.array(y), np.array(z))
+gr.SetName("line")
+gr.Draw("LINE")
+gr.GetXaxis().GetXmax()
+gr.SetTitle("GEANT4")
+leg = ROOT.TLegend(0.4, 0.6, 0.8, 0.85)
+leg.SetNColumns(natoms+1)
+leg.AddEntry(gr, P.name+"#Rightarrow"+Env.molequla.name, "s")
+for i in range(natoms):
+    if type(X[i])==type(1):
+        continue
+    graps[i] = ROOT.TGraph2D(len(X[i]), np.array(X[i]), np.array(Y[i]), np.array(Z[i]))
+    graps[i].SetMarkerColor(i+2)
+    graps[i].SetMarkerStyle(i+20)
+    graps[i].SetName(str(i))
+    graps[i].SetMarkerSize(1.0)
+    graps[i].Draw("PSAME")
+    leg.AddEntry(graps[i], Env.molequla.atoms[i].name, "p")
+
+leg.Draw()
+# gr = ROOT.TGraph2D(len(X), np.array(X), np.array(Y), np.array(Z))
+# gr.Draw("LINE")
+ROOT.gPad.Update()
